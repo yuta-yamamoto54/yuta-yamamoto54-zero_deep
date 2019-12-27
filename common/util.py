81d3aa2d6824c5386c5152304b117899e436774e
YutaYamamoto <yamayuu0301@gmail.com>
2019-12-27 18:48:59 +0900

    7~8章

--- common/util.py
+++ common/util.py
@@ -52,9 +52,12 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     col : 2次元配列
     """
     N, C, H, W = input_data.shape
+    #切り捨て除算
     out_h = (H + 2*pad - filter_h)//stride + 1
     out_w = (W + 2*pad - filter_w)//stride + 1
 
+    #第一引数：N次元の行列　第二引数：前後の文字詰める量　第三引数：埋め込み方式
+    #データ数、チャンネル方向にはパディング０、高さと幅の方向には前後にpad分0パディングする
     img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
     col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
 
